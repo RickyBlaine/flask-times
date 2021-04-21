b1420dddf5963592d907200cbcce2f1ef14e5c7d
Andre Ray <andreray@Andres-MacBook-Pro.local>
2021-04-20 22:55:15 -0400

    Updated w flask 1055

--- headlines.py
+++ headlines.py
@@ -1,8 +1,19 @@
+from flask import Flask,  render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():     
+    return '<h1>Welcome!</h1>'
 
 
-print ("just getting started")
+
+@app.route('/headlines/<nm>')
+
+def hello_name(nm):
+    return render_template('name.html', name=nm)       
 
 
-x = "let's update"
-
-print(x)
+if __name__ == '__main__':  
+    print('starting Flask app', app.name)  
+    app.run(debug=True)
